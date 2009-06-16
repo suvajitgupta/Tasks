b13d08306ad2ded3c3d0b19285c119037327ebf3
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-06-16 19:08:20 -0400

    hooked up Fixtures via ids

--- apps/tasks/fixtures/project.js
+++ apps/tasks/fixtures/project.js
@@ -10,10 +10,12 @@ Tasks.Project.FIXTURES = [];
 
 function generateProjectFixtures() {
 	var fixtures = Tasks.Project.FIXTURES;
-	for (var i = 0; i < 10; i++) {
+	for (var i = 0; i < 5; i++) {
 		var project = new Tasks.Project();
 		project.id = i+1;
 		project.name = "Project" + project.id;
+		var t = 3*i;
+		project.tasks = [ t+1, t+2, t+3 ];
 		fixtures[i] = project;
 	}
 }
