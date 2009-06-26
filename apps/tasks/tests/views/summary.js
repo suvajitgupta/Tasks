631aae3e572bb7b571262c10a3f5b81ae2f1cdab
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-06-26 18:45:15 -0400

    added header to unit test file

--- apps/tasks/tests/views/summary.js
+++ apps/tasks/tests/views/summary.js
@@ -7,8 +7,6 @@
  * @author Suvajit Gupta
  */
 
-
-
 var pane = SC.ControlTestPane.design()
   .add("none", Tasks.SummaryView, {
     value: 0
@@ -29,18 +27,18 @@ module("Tasks.SummaryView tests", pane.standardSetup());
 test("no items", function(){
   var view = pane.view('none');
   ok(view, 'view should render');
-  equals(view.$().get(0).innerHTML,'No projects', 'displays the correct test');
+  equals(view.$().get(0).innerHTML, 'No projects', 'displays the correct test');
 });
 
 test("one item", function(){
   var view = pane.view('one');
-  equals(view.$().get(0).innerHTML,'1 project', 'displays the correct test');
+  equals(view.$().get(0).innerHTML, '1 project', 'displays the correct test');
   
 });
 
 test("many items", function(){
   var view = pane.view('many');
-  equals(view.$().get(0).innerHTML,'15 projects', 'displays the correct test');
+  equals(view.$().get(0).innerHTML, '15 projects', 'displays the correct test');
   
   
 });
