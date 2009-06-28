deb3ddd0b721a89d62c5f7fbcb832ebb8ee9a87b
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-06-27 23:37:48 -0400

    added first model unit test

--- apps/tasks/tests/models/task.js
+++ apps/tasks/tests/models/task.js
@@ -1,15 +1,39 @@
-// ==========================================================================
-// Project:   Tasks.Task Unit Test
-// Copyright: ©2009 My Company, Inc.
-// ==========================================================================
-/*globals Tasks module test ok equals same stop start */
+/**
+ * Unit test for Task model
+ *
+ * @author Suvajit Gupta
+ */
+/*globals Tasks sc_require module test equals ok */
+sc_require('core');
+sc_require('models/task');
 
-module("Tasks.Task");
+var task; // task instance with data
 
-// TODO: Replace with real unit test for Task
-test("test description", function() {
-  var expected = "test";
-  var result   = "test";
-  equals(result, expected, "test should equal test");
+module("Tasks.Task", {
+
+  setup: function() {    
+    var taskHash = {
+      id: 0,
+      name: "Summary for task zero",
+      type: Tasks.TASK_TYPE_FEATURE,
+      description: "Description for task 0" ,
+      priority: Tasks.TASK_PRIORITY_HIGH,
+      status: Tasks.TASK_STATUS_DONE,
+      validation: Tasks.TASK_VALIDATION_PASSED,
+      effort: "5",
+      submitter: 1,
+      assignee: 2
+    };
+    task = Tasks.get('store').createRecord(Tasks.Task, taskHash);
+  },
+
+  teardown: function() {
+    task = null; // reset
+  }
+  
 });
 
+test("Function: displayName()",
+function() {
+  equals(task.get('displayName'), "Summary for task zero {5}", "displayName() should show name with effort appended in curly braces");
+});
