0a22b32600c59d8e42ffe522f8e7b79bee7058b2
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-06-30 16:02:06 -0400

    4 tasks per project in fixtures

--- apps/tasks/fixtures/project.js
+++ apps/tasks/fixtures/project.js
@@ -8,8 +8,8 @@ for (var i = 0; i < 5; i++) {
   var projectHash = {};
   projectHash.id = i+1;
   projectHash.name = "Project" + projectHash.id;
-  var t = 3*i;
-  projectHash.tasks = [ t+1, t+2, t+3 ];
+  var t = 4*i;
+  projectHash.tasks = [ t+1, t+2, t+3, t+4 ];
   if (i < 3) projectHash.timeLeft = t*t;
   Tasks.Project.FIXTURES[i] = projectHash;
 }
