55678a6ef46d6d1c0765d9deb47723a9745a2ad7
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-07-29 09:43:59 -0400

    updated Plan

--- apps/tasks/views/task.js
+++ apps/tasks/views/task.js
@@ -22,42 +22,42 @@ Tasks.TaskView = SC.ListItemView.extend(
       
       var priority = content.get('priority');
       switch(priority){
-        case CoreTasks.TASK_PRIORITY_HIGH:
+        case Tasks.TASK_PRIORITY_HIGH:
           context.addClass('tasks-priority-high');
           break;
-        case CoreTasks.TASK_PRIORITY_MEDIUM:
+        case Tasks.TASK_PRIORITY_MEDIUM:
           context.addClass('tasks-priority-medium');
           break;
-        case CoreTasks.TASK_PRIORITY_LOW:
+        case Tasks.TASK_PRIORITY_LOW:
           context.addClass('tasks-priority-low');
           break;          
       }
       
       var status = content.get('status');
       switch(status){
-        case CoreTasks.TASK_STATUS_PLANNED:
+        case Tasks.TASK_STATUS_PLANNED:
           context.addClass('tasks-status-planned');
           break;
-        case CoreTasks.TASK_STATUS_ACTIVE:
+        case Tasks.TASK_STATUS_ACTIVE:
           context.addClass('tasks-status-active');
           break;
-        case CoreTasks.TASK_STATUS_DONE:
+        case Tasks.TASK_STATUS_DONE:
           context.addClass('tasks-status-done');
           break;          
-        case CoreTasks.TASK_STATUS_RISKY:
+        case Tasks.TASK_STATUS_RISKY:
           context.addClass('tasks-status-risky');
           break;          
       }
       
       var validation = content.get('validation');
       switch(validation){
-        case CoreTasks.TASK_VALIDATION_UNTESTED:
+        case Tasks.TASK_VALIDATION_UNTESTED:
           context.addClass('tasks-validation-untested');
           break;
-        case CoreTasks.TASK_VALIDATION_PASSED:
+        case Tasks.TASK_VALIDATION_PASSED:
           context.addClass('tasks-validation-passed');
           break;
-        case CoreTasks.TASK_VALIDATION_FAILED:
+        case Tasks.TASK_VALIDATION_FAILED:
           context.addClass('tasks-validation-failed');
           break;          
       }
