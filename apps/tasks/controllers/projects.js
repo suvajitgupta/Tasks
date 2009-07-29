1c7e38d94aabc069e47b0d45d8f20478f05f20a6
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-07-28 23:52:13 -0400

    deleted dead code

--- apps/tasks/controllers/projects.js
+++ apps/tasks/controllers/projects.js
@@ -10,17 +10,6 @@ Tasks.projectsController = SC.ArrayController.create(
 /** @scope Tasks.projectsController.prototype */ {
   
   allowsMultipleSelection: NO,
-  allowsEmptySelection: NO,
-  
-  getTasksByProjectId: function(projectId){
-    if(!projectId) return;
-    var q = SC.Query.create({
-      recordType: CoreTasks.Project,
-      conditions: "id = '%@'".fmt(projectId)      
-    });
-    var project = CoreTasks.get('store').findAll(q).firstObject();
-    var tasks = project.get('tasks');
-    return tasks;
-  }
+  allowsEmptySelection: NO
   
 });
