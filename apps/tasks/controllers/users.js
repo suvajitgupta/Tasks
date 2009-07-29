be351e4a473718c07c202ae0f78c698c0ee65071
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-07-29 18:56:10 -0400

    added AllProjects & started adding usersController

---
+++ apps/tasks/controllers/users.js
@@ -0,0 +1,12 @@
+/*globals Tasks */
+
+/** 
+  This is the controller for the user select field view.
+  
+  @extends SC.ArrayController
+  @author Suvajit Gupta
+*/
+Tasks.usersController = SC.ArrayController.create(
+/** @scope Tasks.usersController.prototype */ {
+
+});
