8a33f7f71558aca95df9a3c08d75c9a832218bf0
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-08-11 23:02:26 -0400

    created exportDataController

--- apps/tasks/core_actions.js
+++ apps/tasks/core_actions.js
@@ -253,49 +253,7 @@ Tasks.mixin({
    * Export data to external text file.
    */
   exportData: function() {
-    var val, task, user;
-    var data = "# Tasks data export at " + new Date().format('MMM dd, yyyy hh:mm:ssa') + '\n\n';
-    var pc = this.get('projectsController');
-
-    pc.forEach(function(rec){
-          if (rec.get('name') === CoreTasks.ALL_TASKS_NAME) return;
-          var tasks = rec.get('tasks');
-          var len = tasks.get('length');
-          if(rec.get('name') !== CoreTasks.INBOX_NAME) {
-            data += rec.get('displayName') + '\n';
-          }
-          for (var i = 0; i < len; i++) {
-            task = tasks.objectAt(i);
-            switch(task.get('priority')) {
-              case CoreTasks.TASK_PRIORITY_HIGH: val = '^'; break;
-              case CoreTasks.TASK_PRIORITY_MEDIUM: val = '-'; break;
-              case CoreTasks.TASK_PRIORITY_LOW: val = 'v'; break;
-            }
-            data += val + ' ';
-            data += task.get('displayName');
-            user = task.get('submitter');
-            if (user) data += ' <' + user.get('name') + '>';
-            user = task.get('assignee');
-            if (user) data += ' [' + user.get('name') + ']';
-            val = task.get('type');
-            if(val !== CoreTasks.TASK_TYPE_OTHER)  data += ' $' + val;
-            val = task.get('status');
-            if(val !== CoreTasks.TASK_STATUS_PLANNED)  data += ' @' + val;
-            val = task.get('validation');
-            if(val !== CoreTasks.TASK_VALIDATION_UNTESTED)  data += ' %' + val;
-            val = task.get('description');
-            if(val) {
-              var lines = val.split('\n');
-              for (var j = 0; j < lines.length; j++) {
-                data += '\n| ' + lines[j];
-              }
-            }
-            data += '\n';
-          }
-          data += '\n';
-      }, pc);
-    
-    console.log(data);
+    Tasks.exportDataController.exportData();  
   },
   
   /**
