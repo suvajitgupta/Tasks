227b4b38f49d1c1c39893fa4a2565129a27fa9ce
Sean Eidemiller <sean@halcyon.cc>
2009-08-29 15:16:11 -0400

    Changed Persevere port (8080 -> 8088).

--- server-start.sh
+++ server-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DIR=`pwd`
 cd tasks-server
-"$DIR"/persevere/bin/persvr --base-uri /tasks-server > "$DIR"/server.out 2>&1 &
+"$DIR"/persevere/bin/persvr -p 8088 --base-uri /tasks-server > "$DIR"/server.out 2>&1 &
 cd "$DIR"
