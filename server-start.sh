6245ed777b518de48f8c7d8ceba169e2c5e71a3a
Sean Eidemiller <sean@halcyon.cc>
2009-08-30 20:28:53 -0400

    Hardened Persevere start/stop scripts.

--- server-start.sh
+++ server-start.sh
@@ -1,5 +1,39 @@
 #!/bin/sh
+# A simple shell script that launches the Persevere server.
+# Author: Sean Eidemiller
+
 DIR=`pwd`
+
+if [ -f /bin/echo ]; then
+  ECHO="/bin/echo"
+else
+  ECHO="echo"
+fi
+
+"$ECHO" -n "Starting Persevere server... "
+
+if [ ! -d tasks-server ]; then
+  echo "FAILED"
+  echo "Unable to locate tasks-server directory."
+  exit 1
+fi
+
 cd tasks-server
-"$DIR"/persevere/bin/persvr -p 8088 --base-uri /tasks-server > "$DIR"/server.out 2>&1 &
+
+"$DIR"/persevere/bin/persvr -p 8088 --base-uri /tasks-server > "$DIR"/server.out 2> server.err &
+
+if [ "$?" -eq 0 ]; then
+  echo "OK"
+  echo "Server PID: $!"
+else
+  echo "FAILED"
+  cat server.err
+  rm server.err
+  exit 1
+fi
+
+if [ -f server.err ]; then
+  rm server.err
+fi
+
 cd "$DIR"
--- server-stop.sh
+++ server-stop.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
+# A simple script for stopping the Persevere server.
+# Author: Sean Eidemiller
+
+if [ -f /bin/echo ]; then
+  ECHO="/bin/echo"
+else
+  ECHO="echo"
+fi
+
+"$ECHO" -n "Stopping Persevere server... "
+
 PID=`ps aux | grep java | grep persevere | grep -v grep | awk '{print $2}'`
+
 if [ ! -z "$PID" ]; then
   kill "$PID"
+else
+  echo "FAILED"
+  echo "The server doesn't appear to be running."
+  exit 1
+fi
+
+if [ "$?" -eq 0 ]; then
+  echo "OK"
+else
+  echo "FAILED"
+  echo "Unable to kill server process."
+  exit 1
 fi
