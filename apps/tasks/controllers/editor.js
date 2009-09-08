e8e6e26142e7c66d6de572d3822f37478f17ee0f
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-09-08 18:54:14 -0400

    started refining task editor

---
+++ apps/tasks/controllers/editor.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project: Tasks
+// ==========================================================================
+/*globals Tasks */
+
+/** 
+
+  This controller manages editing of a task
+
+  @extends SC.ObjectController
+	@author Suvajit Gupta
+*/
+Tasks.editorController = SC.ObjectController.create(
+/** @scope Tasks.projectController.prototype */ {
+  
+  content: null
+
+});
