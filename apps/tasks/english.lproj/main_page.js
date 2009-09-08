69d42276ab920e52a4cc05bf12baf1e71b81abd5
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-09-07 20:24:55 -0400

    made Welcome View wider

--- apps/tasks/english.lproj/main_page.js
+++ apps/tasks/english.lproj/main_page.js
@@ -38,7 +38,7 @@ Tasks.mainPage = SC.Page.design({
         }),
 
         Tasks.WelcomeView.design({
-          layout: { centerY: 0, height: 20, left: 270, width: 200 },
+          layout: { centerY: 0, height: 20, left: 270, width: 300 },
           classNames: ['welcome-label'],
           valueBinding: SC.Binding.oneWay('CoreTasks.user')
         }),
