be719c32e244904abd0159475251d29c26eb5ea9
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-09-15 14:50:00 -0400

    updated Tasks Plan and deleted user fixtures

--- frameworks/core-tasks/fixtures/user.js
+++ frameworks/core-tasks/fixtures/user.js
@@ -23,88 +23,28 @@ CoreTasks.User.FIXTURES = [
   loginName: "JH2" },
 
   { id: 4,
-  name: "Mike Ball",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "MB" },
-
-  { id: 5,
   name: "Brandon Blatnick",
   role: CoreTasks.USER_ROLE_DEVELOPER,
   loginName: "BB" },
 
-  { id: 6,
-  name: "Erich Ocean",
+  { id: 5,
+  name: "Mike Ball",
   role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "EO" },
+  loginName: "MB" },
 
   { id: 7,
-  name: "Evin Grano",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "EG" },
-
-  { id: 8,
   name: "SproutCore",
   role: CoreTasks.USER_ROLE_DEVELOPER,
   loginName: "SC" },
 
-  { id: 9,
+  { id: 8,
   name: "Matt Grantham",
   role: CoreTasks.USER_ROLE_DEVELOPER,
   loginName: "MG" },
 
-  { id: 10,
-  name: "Mo Taher",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "MT" },
-
-  { id: 11,
-  name: "Dimitri Colomvakos",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "DC" },
-
-  { id: 12,
+  { id: 9,
   name: "Bill Mosteller",
   role: CoreTasks.USER_ROLE_TESTER,
-  loginName: "BM" },
-
-  { id: 13,
-  name: "Jonathan Lewis",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "JL" },
-
-  { id: 14,
-  name: "Sebastian Feher",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "SF2" },
-
-  { id: 15,
-  name: "Fred Sakr",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "FS" },
-
-  { id: 16,
-  name: "Michael Scrivo",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "MS" },
-
-  { id: 17,
-  name: "Scott Hill",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "SH" },
-
-  { id: 18,
-  name: "Andrew Robinson",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "AR" },
-
-  { id: 19,
-  name: "Michael Cohen",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "MC" },
-
-  { id: 20,
-  name: "Michael Harris",
-  role: CoreTasks.USER_ROLE_DEVELOPER,
-  loginName: "MH" }
+  loginName: "BM" }
 
 ];
