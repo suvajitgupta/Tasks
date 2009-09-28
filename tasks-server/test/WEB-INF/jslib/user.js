ac05ab79e67ef4e6f45fc4afbf2e9cde24046408
Sean Eidemiller <sean@halcyon.cc>
2009-09-28 09:30:21 -0400

    Relaxed the string enforcement on the developer attribute in the Persevere data model.

--- tasks-server/test/WEB-INF/jslib/user.js
+++ tasks-server/test/WEB-INF/jslib/user.js
@@ -3,7 +3,7 @@ Class({
   properties: {
     name: { type: "any", optional: false },
     loginName: { type: "any", optional: false },
-    role: { type: "any", optional: false, enum: ["Manager", "Developer", "Tester"] },
+    role: { type: "any", optional: false },
     preferences: { type: "object", optional: true },
     authToken: { type: "any", optional: true }
   }
