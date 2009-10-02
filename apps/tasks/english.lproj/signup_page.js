8de468a67fec8f3251fa274a9cbf24378bc5dd4d
Joshua Holt <holt.josh@gmail.com>
2009-10-02 16:48:00 -0400

    Start of Signup page.
    Signed-off-by: Joshua Holt <holt.josh@gmail.com>

---
+++ apps/tasks/english.lproj/signup_page.js
@@ -0,0 +1,19 @@
+//============================================================================
+// Tasks.signupPage
+//============================================================================
+sc_require('core');
+/*globals Tasks*/
+/**
+
+  This is the signup page for Tasks
+  
+  @extends SC.Page
+  @author Joshua Holt
+  @version preBeta
+  @since preBeta
+
+*/
+
+Tasks.signupPage = SC.Page.create({
+  // TODO: [JH2] Implement Sign up page and logic.
+});
