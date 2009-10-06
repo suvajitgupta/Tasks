89d89d1c073257d1173a2f75308b2ab042c6da3c
Joshua Holt <holt.josh@gmail.com>
2009-10-06 03:24:27 -0400

    Added place holder code for tokyo cabinet (Future use maybe?)

--- sinatra-server.rb
+++ sinatra-server.rb
@@ -14,51 +14,60 @@ require 'dm-core'
 require 'dm-validations'
 require 'dm-timestamps'
 require 'dm-types'
+#require 'dm-tokyo-adapter'
 require 'json'
-
-#-----------------------------------------------------------------------------
-# USED TO SET CONTENT TYPE
-#
-mime :json, "application/json"
 
 #-----------------------------------------------------------------------------
 # MODELS
 #
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/Tasks.sqlite3")
 
+# USE TOKYO CABINET WHEN it's more stable
+# DataMapper.setup(:default,
+#     :adapter  => 'tokyo_cabinet',
+#     :database => 'tc',
+#     :path     => File.dirname(__FILE__)
+# )
+
 class User
   include DataMapper::Resource
 
-  property  :id,              Serial
-  property  :name,            Text,   :nullable =>  false  
-  property  :'loginName',     Text,   :nullable =>  false  
-  property  :role,            Enum[:Manager, :Developer, :Tester, :Guest], :default => :Guest
-  property  :'emailAddress',  Text,   :nullable =>  false
-  property  :password,        Text,   :nullable =>  false
-  property  :preferences,     Text
-  property  :'authToken',     Text
-  property  :created_at,      DateTime
-  property  :updated_at,      DateTime
+  property  :id,            Serial
+  property  :name,          Text,   :nullable =>  false  
+  property  :loginName,     Text,   :nullable =>  false  
+  property  :role,          String, :default => '_Guest'
+  property  :emailAddress,  Text #,   :nullable =>  false -- NOT IMPLEMENTED IN ALL AREAS
+  property  :password,      Text #,   :nullable =>  false -- NOT IMPLEMENTED IN ALL AREAS
+  property  :preferences,   Json
+  property  :authToken,     Text
+  # property  :created_at,      DateTime -- NOT IMPLEMENTED
+  # property  :updated_at,      DateTime -- NOT IMPLEMENTED
   
   def url
     "user/#{self.id}"    
   end
   
-  def to_json(*a)
+  def to_json()
     ret = {}
     self.attributes.each do |k,v|
       ret[k] = k.to_s == "id" ? self.url : v unless k.to_s == "password"
     end
-    ret.to_json(*a)
+    ret.to_json()
   end
   
-  @required = [:name, :'loginName', :password, :'emailAddress']
+  # @required = [:name, :loginName, :password, :emailAddress] -- waiting impelmentation
+  @required = [:name, :loginName, :role]
+  @required_for_update = [:name, :loginName]
   
-  def self.parse_json(body)
+  def self.parse_json(body, requestMethod)
     json = JSON.parse(body)
-    ret = { :name => json['name'], :'loginName' => json['loginName'], 
-            :password => json['password'], :'emailAddress' => json['emailAddress'] }            
-    return nil if @required.find { |r| ret[r].nil? }
+    ret = {}
+    json.each {|k,v| ret[k.to_sym] = v unless k == 'id' }
+    if requestMethod === 'update'
+      return nil if @required_for_update.find { |r| ret[r].nil? }
+    else
+      return nil if @required.find { |r| ret[r].nil? }
+    end
     ret
   end
 end
@@ -69,25 +78,26 @@ class Project
   property :id,         Serial
   property :name,       Text, :nullable => false
   property :'timeLeft', Integer
-  property :tasks,      Text
+  property :tasks,      Json
   
   def url
     "project/#{self.id}"
   end
   
-  def to_json(*a)
+  def to_json()
     ret = {}
     self.attributes.each do |k,v|
       ret[k] = k.to_s == "id" ? self.url : v
     end
-    ret.to_json(*a)
+    ret.to_json()
   end
   
   @required = [:name]
   
-  def self.parse_json(body)
+  def self.parse_json(body, requestMethod)
     json = JSON.parse(body)
-    ret = { :name => json['name'] }
+    ret = {}
+    json.each {|k,v| ret[k.to_sym] = v unless k == 'id' }
     return nil if @required.find { |r| ret[r].nil? }
     ret
   end
@@ -98,35 +108,36 @@ class Task
   include DataMapper::Resource
   
   property  :id,           Serial
-  property  :name,         Text, :nullable => false
-	property  :priority,     Enum[:HIGH, :MEDIUM, :LOW], :default => :MEDIUM
-	property  :effort,       Integer
+  property  :name,         Text,   :nullable => false
+	property  :priority,     String, :default => '_Medium'
+	property  :effort,       String
 	property  :submitter,    Integer
 	property  :assignee,     Integer
-	property  :type,         Enum[:Feature,  :Bug,     :Other],          :default  => :Other
-	property  :status,       Enum[:Planned,  :Active,  :Done,  :Risky],  :default  => :Planned
-	property  :validation,   Enum[:Untested, :Passed,  :Failed],         :default  => :Untested
+	property  :type,         String, :default => '_Other'
+	property  :status,       String, :default => '_Planned'
+	property  :validation,   String, :default => '_Untested'
 	property  :description,  Text
-	property  :created_at,   DateTime, :default  => DateTime.now
-  property  :updated_at,   DateTime, :default  => DateTime.now
+  # property  :created_at,   DateTime, :default  => DateTime.now -- NOT IMPLEMENTED
+  # property  :updated_at,   DateTime, :default  => DateTime.now -- NOT IMPLEMENTED
   
   def url
     "task/#{self.id}"
   end
   
-  def to_json(*a)
+  def to_json()
     ret = {}
     self.attributes.each do |k,v|
       ret[k] = k.to_s == "id" ? self.url : v
     end
-    ret.to_json(*a)
+    ret.to_json()
   end
   
   @required = [:name]
   
-  def self.parse_json(body)
+  def self.parse_json(body, requestMethod)
     json = JSON.parse(body)
-    ret = {:name => json['name']}
+    ret = {}
+    json.each {|k,v| ret[k.to_sym] = v unless k == 'id' }
     return nil if @required.find { |r| ret[r].nil? }
     ret
   end
@@ -142,16 +153,16 @@ def json_get_list(route, options={})
   get(route, options) do 
     # var used to hold the list of model_instances from the DB
     list = []
-    
+    modelType = params[:model]
     # Determine the model and Find all records.
-    case params[:captures].first
-      when 'user':    list = User.all
-      when 'task':    list = Task.all
-      when 'project': list = Project.all
+    case modelType
+      when 'user' then    list = User.all
+      when 'task' then    list = Task.all
+      when 'project' then list = Project.all
     end
     
-    content_type :json
-    Array(list).to_json
+    content_type "application/json"
+    list.length > 0 ? Array(list).to_json : [].to_json
       
   end
 end
@@ -161,18 +172,18 @@ def json_get_single(route, options={})
     
     # holds the desired model instance
     mobj = {}
-    modelType = params[:captures].first
-    modelId   = params[:captures][1]
+    modelType = params[:model]
+    modelId   = params[:id]
     # Determine Which model class we need to search and set mobj
     case modelType
-      when 'user':    mobj = User.get(modelId)    rescue nil
-      when 'task':    mobj = Task.get(modelId)    rescue nil
-      when 'project': mobj = Project.get(modelId) rescue nil
+      when 'user' then    mobj = User.get(modelId)    rescue nil
+      when 'task' then    mobj = Task.get(modelId)    rescue nil
+      when 'project' then mobj = Project.get(modelId) rescue nil
     end
     
     # Repond with error 404 or JSON
     halt 404, "#{modelType[modelId]} not found" if mobj.nil?
-    content_type :json
+    content_type "application/json"
     mobj.to_json
     
   end
@@ -183,20 +194,20 @@ def json_post(route, options={})
     
     opts = {}
     newRecord = {}
-    modelType = params[:captures].first
+    modelType = params[:model]
     
     case modelType
-      when 'user':    opts = User.parse_json(request.body.read)     rescue nil
-      when 'task':    opts = Task.parse_json(request.body.read)     rescue nil
-      when 'project': opts = Project.parse_json(request.body.read)  rescue nil
+      when 'user' then    opts = User.parse_json(request.body.read, 'post')     rescue nil
+      when 'task' then    opts = Task.parse_json(request.body.read, 'post')     rescue nil
+      when 'project' then opts = Project.parse_json(request.body.read, 'post')  rescue nil
     end
     
     halt 404, "Invalid JSON" if opts.nil?
     
     case modelType
-      when 'user': newRecord    = User.new(opts)
-      when 'task': newRecord    = Task.new(opts)
-      when 'project': newRecord = Project.new(opts)
+      when 'user' then newRecord    = User.new(opts)
+      when 'task' then newRecord    = Task.new(opts)
+      when 'project' then newRecord = Project.new(opts)
     end
     
     halt(500, "Could not save #{modelType}" ) unless newRecord.save
@@ -213,28 +224,28 @@ def json_put(route, options={})
     
     record = {}
     opts = {}
-    modelType = params[:captures].first
-    modelId   = params[:captures][1]
+    modelType = params[:model]
+    modelId   = params[:id]
     
     case modelType
-      when 'user':    record = User.get(modelId)    rescue nil
-      when 'task':    record = Task.get(modelId)    rescue nil
-      when 'project': record = Project.get(modelId) rescue nil
+      when 'user' then    record = User.get(modelId)    rescue nil
+      when 'task' then    record = Task.get(modelId)    rescue nil
+      when 'project' then record = Project.get(modelId) rescue nil
     end
     
     halt(404, 'Not Found') if record.nil?
 
     case modelType
-      when 'user':    opts = User.parse_json(request.body.read)     rescue nil
-      when 'task':    opts = Task.parse_json(request.body.read)     rescue nil
-      when 'project': opts = Project.parse_json(request.body.read)  rescue nil
+      when 'user' then    opts = User.parse_json(request.body.read, 'update')     rescue nil
+      when 'task' then    opts = Task.parse_json(request.body.read, 'update')     rescue nil
+      when 'project' then opts = Project.parse_json(request.body.read, 'update')  rescue nil
     end
     
     halt 404, "Invalid JSON" if opts.nil?
     
     halt(500, "Could not update #{modelType}" ) unless record.update(opts)
 
-    response['Content-Type'] = :json
+    response['Content-Type'] = "application/json"
     record.to_json
     
   end
@@ -244,13 +255,13 @@ def json_delete(route, options={})
   delete(route, options) do
     
     record = {}
-    modelType = params[:captures].first
-    modelId   = params[:captures][1]
+    modelType = params[:model]
+    modelId   = params[:id]
     
     case modelType
-      when 'user':    record = User.get(modelId)    rescue nil
-      when 'task':    record = Task.get(modelId)    rescue nil
-      when 'project': record = Project.get(modelId) rescue nil
+      when 'user' then  record = User.get(modelId)    rescue nil
+      when 'task' then    record = Task.get(modelId)    rescue nil
+      when 'project' then record = Project.get(modelId) rescue nil
     end
     
     record.destroy unless record.nil?
@@ -261,12 +272,26 @@ end
 #-----------------------------------------------------------------------------
 # ROUTES
 #
-json_get_list   %r{/tasks-server/([\w]+)$}
+# REGEX VERSIONS
+#
+# json_get_list   %r{/tasks-server/([\w]+)$}
+# 
+# json_get_single %r{/tasks-server/([\w]+)/([\w+])$}
+# 
+# json_post       %r{/tasks-server/([\w]+)}
+# 
+# json_put        %r{/tasks-server/([\w]+)/([\w+])$}
+# 
+# json_delete     %r{/tasks-server/([\w]+)/([\w+])$}
 
-json_get_single %r{/tasks-server/([\w]+)/([\w+])$}
+# NAMED PARAM VERSIONS
+#
+json_get_list   '/tasks-server/:model'
 
-json_post       %r{/tasks-server/([\w]+)}
+json_get_single '/tasks-server/:model/:id'
 
-json_put        %r{/tasks-server/([\w]+)/([\w+])$}
+json_post       '/tasks-server/:model'
 
-json_delete     %r{/tasks-server/([\w]+)/([\w+])$}
+json_put        '/tasks-server/:model/:id'
+
+json_delete     '/tasks-server/:model/:id'
