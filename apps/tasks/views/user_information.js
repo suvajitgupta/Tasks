658038d854a1d107186612aab53dc33107946933
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-10-23 20:34:31 -0400

    changed per Erich

--- apps/tasks/views/user_information.js
+++ apps/tasks/views/user_information.js
@@ -18,63 +18,63 @@ Tasks.UserInformationView = SC.View.extend(
     
     var childViews = [];
     
-    childViews.push(this.createChildView(SC.LabelView.design({
+    childViews.push(this.createChildView(SC.LabelView.extend({
       layout: { top: 10, left: 0, width: 85, height: 18 },
       textAlign: SC.ALIGN_RIGHT,
       value: "_LoginName:".loc() 
     })));
-    childViews.push(this.createChildView(SC.TextFieldView.design({
+    childViews.push(this.createChildView(SC.TextFieldView.extend({
       layout: { top: 10, left: 90, height: 20, width: 200 },
       hint: "_Initials".loc(),
-      valueBinding: SC.binding('.content.loginName', this).toLocale()
+      valueBinding: SC.binding('*.content.loginName', this).toLocale()
     })));
     
-    childViews.push(this.createChildView(SC.LabelView.design({
+    childViews.push(this.createChildView(SC.LabelView.extend({
       layout: { top: 42, left: 0, width: 85, height: 18 },
       textAlign: SC.ALIGN_RIGHT,
       value: "_FullName:".loc() 
     })));
-    childViews.push(this.createChildView(SC.TextFieldView.design({
+    childViews.push(this.createChildView(SC.TextFieldView.extend({
       layout: { top: 42, left: 90, height: 20, width: 200 },
       hint: "_FirstLast".loc(),
-      valueBinding: SC.binding('.content.name', this).toLocale()
+      valueBinding: SC.binding('*.content.name', this).toLocale()
     })));
     
-    childViews.push(this.createChildView(SC.LabelView.design({
+    childViews.push(this.createChildView(SC.LabelView.extend({
       layout: { top: 74, left: 0, width: 85, height: 18 },
       textAlign: SC.ALIGN_RIGHT,
       value: "_Role:".loc()
     })));
-    childViews.push(this.createChildView(SC.SelectFieldView.design({
+    childViews.push(this.createChildView(SC.SelectFieldView.extend({
       layout: { top: 74, left: 90, height: 20, width: 200 },
       localize: YES,
       objects: CoreTasks.roles,
-      valueBinding: SC.binding('.content.role', this)
+      valueBinding: SC.binding('*.content.role', this)
     })));
     
-    childViews.push(this.createChildView(SC.LabelView.design({
+    childViews.push(this.createChildView(SC.LabelView.extend({
       layout: { top: 106, left: 0, width: 85, height: 18 },
       textAlign: SC.ALIGN_RIGHT,
       value: "_Email:".loc()
     })));
-    childViews.push(this.createChildView(SC.TextFieldView.design(SC.Validatable,{
+    childViews.push(this.createChildView(SC.TextFieldView.extend(SC.Validatable,{
       layout: { top: 106, left: 90, height: 20, width: 275 },
       validator: SC.Validator.EmailOrEmpty,
       errorLabel: "_InvalidEmailAddress".loc(),
       hint: "_EmailAddress".loc(),
-      valueBinding: SC.binding('.content.emailAddress', this).toLocale()
+      valueBinding: SC.binding('*.content.emailAddress', this).toLocale()
     })));
     
-    childViews.push(this.createChildView(SC.LabelView.design({
+    childViews.push(this.createChildView(SC.LabelView.extend({
       layout: { top: 138, left: 0, width: 85, height: 18 },
       textAlign: SC.ALIGN_RIGHT,
       value: "_Password:".loc()
     })));
-    childViews.push(this.createChildView(SC.TextFieldView.design({
+    childViews.push(this.createChildView(SC.TextFieldView.extend({
       layout: { top: 138, left: 90, height: 20, width: 200 },
       hint: "_PasswordHint".loc(),
       isPassword: YES,
-      valueBinding: SC.binding('.content.password', this).toLocale()
+      valueBinding: SC.binding('*.content.password', this).toLocale()
     })));
     
     this.set('childViews', childViews);
