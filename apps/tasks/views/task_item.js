a0eff57fe7cb8a1f58f4612bc3cce18ffa0ad0d4
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-10-27 23:52:48 -0400

    added FIXME

--- apps/tasks/views/task_item.js
+++ apps/tasks/views/task_item.js
@@ -18,6 +18,7 @@ Tasks.TaskItemView = SC.ListItemView.extend(
   content: null,
   _editorPane: null,
   
+  // FIXME: [SG] Beta: decide how to handle null value of task.submitterId/assigneeId/projectId
   _listUsers: function() {
     return SC.Binding.transform(function(value, binding) {
        var ret = value.toArray();
