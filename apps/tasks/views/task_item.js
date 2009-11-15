c2e1ac594a9e94ad2301542d0aa54c2307e64438
Suvajit Gupta <suvajit.gupta@eloqua.com>
2009-11-14 22:16:34 -0500

    updated Tasks Plan, right justlfied Task ID

--- apps/tasks/views/task_item.js
+++ apps/tasks/views/task_item.js
@@ -52,7 +52,7 @@ Tasks.TaskItemView = SC.ListItemView.extend(
       var that = this;
       this._editorPane = SC.PickerPane.create({
         
-        layout: { width: 500, height: 200 },
+        layout: { width: 550, height: 230 },
         
         // Avoid popup panel coming up on other items while it is up already
         poppedUp: false,
