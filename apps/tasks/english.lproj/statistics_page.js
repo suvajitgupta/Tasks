5c92d03425eea8ba9477542744b8a458dc967de8
Suvajit Gupta <suvajit.gupta@eloqua.com>
2010-01-18 20:51:12 -0500

    used transform() in mapping project name to Statistics panel

--- apps/tasks/english.lproj/statistics_page.js
+++ apps/tasks/english.lproj/statistics_page.js
@@ -22,7 +22,7 @@ Tasks.statisticsPane = SC.PanelPane.create({
   contentView: SC.View.design({
     
     layout: { top: 0, left: 0, bottom: 0, right: 0 },
-    childViews: 'titlebar projectNameLabel projectName statistics closeButton'.w(),
+    childViews: 'titlebar projectName statistics closeButton'.w(),
     
     titlebar: SC.View.design(SC.Border, {
       layout: { left: 10, right: 10, top: 10, height: 35 },
@@ -36,19 +36,18 @@ Tasks.statisticsPane = SC.PanelPane.create({
       ]
     }),
     
-    projectNameLabel: SC.LabelView.design({
-      layout: { top: 50, left: 10, height: 20, width: 50 },
-      value: "_Project:".loc()
-    }),
-    
     projectName: SC.LabelView.design({
-      layout: { top: 50, left: 60, height: 20, right: 10 },
-      fontWeight: SC.BOLD_WEIGHT,
-      valueBinding: 'Tasks.projectController*content.name'
+      layout: { top: 50, left: 10, height: 20, right: 10 },
+      textAlign: SC.ALIGN_CENTER,
+      escapeHTML: NO,
+      valueBinding: SC.Binding.transform(function(value, binding) {
+         return "_Project:".loc() + '<b>' + value + '</b>';
+      }).from('Tasks.projectController*content.name')
     }),
     
     statistics: SC.LabelView.design({
       layout: { top: 75, left: 10, right: 10, bottom: 40 },
+      textAlign: SC.ALIGN_CENTER,
       escapeHTML: NO,
       valueBinding: 'Tasks.projectController.projectStatistics'
     }),
