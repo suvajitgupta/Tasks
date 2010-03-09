718f99c6d46eceac732e955cc39046c3f71fe166
Suvajit Gupta <suvajit.gupta@eloqua.com>
2010-03-09 18:51:47 -0500

    took out logicalAnd binding now that it is in SC

--- apps/tasks/mixins/permissible.js
+++ apps/tasks/mixins/permissible.js
@@ -36,34 +36,3 @@ Tasks.Permissible = {
   }
   
 };
-
-/**
-  Adds a transform that forwards the logical 'AND' of values at 'pathA' and
-  'pathB' whenever either source changes.  Note that the transform acts strictly
-  as a one-way binding, working only in the direction
-  
-    'pathA' AND 'pathB' --> value  (value will be either YES or NO)
-
-  Usage example where a delete button's 'isEnabled' value is determined by whether
-  something is selected in a list and whether the current user is allowed to delete:
-  
-    deleteButton: SC.ButtonView.design({
-      isEnabledBinding: SC.Binding.logicalAnd('MyApp.itemsController.hasSelection', 'MyApp.userController.canDelete')
-    })
-
-*/
-SC.Binding.logicalAnd = function(pathA, pathB) {
-
-  // create an object to do the logical computation
-  var gate = SC.Object.create({
-    valueABinding: pathA,
-    valueBBinding: pathB,
-
-    and: function() {
-      return (this.get('valueA') && this.get('valueB')) ? YES : NO;
-    }.property('valueA', 'valueB').cacheable()
-  });
-
-  // add a transform that depends on the result of that computation.
-  return this.from('and', gate).oneWay();
-};
