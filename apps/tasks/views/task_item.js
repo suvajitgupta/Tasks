7620074342502d164755a4fbd2b020887b5ab307
Suvajit Gupta <suvajit.gupta@eloqua.com>
2010-05-12 00:36:08 -0400

    added FIXME for escaping descriptions with metachars for tooltip display

--- apps/tasks/views/task_item.js
+++ apps/tasks/views/task_item.js
@@ -621,12 +621,12 @@ Tasks.TaskItemView = SC.ListItemView.extend(
     }
     
     // Indicate which items have a description
-    // FIXME: [SG] uncomment lines below once you figure out how to escape metachars in description which is messing up display
-    // var description = content.get('description');
-    // if(description) {
-    //   context = context.begin('div').addClass('description-icon')
-    //               .attr('title', description).attr('alt', description).end();
-    // }
+    var description = content.get('description');
+    if(description) {
+      context = context.begin('div').addClass('description-icon').end();
+      // FIXME: [SG] uncomment line below and end() above once you figure out how to escape metachars in description which is messing up display
+                  // .attr('title', description).attr('alt', description).end();
+    }
 
   },
 
