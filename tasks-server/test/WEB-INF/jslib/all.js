fc28f7df390db4c445cae137d209be80bac78c5e
Suvajit Gupta <suvajit.gupta@eloqua.com>
2010-08-10 18:53:21 -0400

    support watches on Persevere test version

--- tasks-server/test/WEB-INF/jslib/all.js
+++ tasks-server/test/WEB-INF/jslib/all.js
@@ -1,6 +1,6 @@
 Class({
   id: "all",
   get: function() {
-    return { users: load('user'), projects: load('project'), tasks: load('task'), tasks: load('watch') }
+    return { users: load('user'), projects: load('project'), tasks: load('task'), watches: load('watch') }
   }
 });
