df643440082d2f560e8a8ffc2fd842a3a0bf4084
Suvajit Gupta <suvajit.gupta@eloqua.com>
2010-08-14 22:37:56 -0400

    fixed bug with new task creation calling popup editor

--- apps/tasks/controllers/tasks.js
+++ apps/tasks/controllers/tasks.js
@@ -283,7 +283,7 @@ Tasks.tasksController = SC.TreeController.create(
     var idx = listView.get('content').indexOf(task);
     // console.log('DEBUG: tasksController.editNewTask at index: ' + idx);
     var listItem = listView.itemViewForContentIndex(idx);
-    if(listItem) listItem.popupEditor();
+    if(listItem) listItem.popupEditor(task);
   }
 
 });
