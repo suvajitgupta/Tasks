d25970a82c45fef9b92d4a81bf50569329d0b0d8
Suvajit Gupta <suvajit.gupta@eloqua.com>
2010-10-17 20:17:22 -0400

    set min height/width on mainPane

--- apps/tasks/english.lproj/main_page.js
+++ apps/tasks/english.lproj/main_page.js
@@ -108,7 +108,7 @@ Tasks.mainPage = SC.Page.design({
 
    masterDetailView: SC.MasterDetailView.design({
      
-     layout: { top: 0, left: 0, right: 0, bottom: 0 },
+     layout: { top: 0, left: 0, right: 0, bottom: 0, minWidth: 1000, minHeight: 500 },
      masterWidth: 250,
      
      masterView: SC.WorkspaceView.extend({
