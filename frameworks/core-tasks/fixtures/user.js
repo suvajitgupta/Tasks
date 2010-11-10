12731acdbfc90743155e7d4e43fc7e3207c29d30
Suvajit Gupta <suvajit.gupta@eloqua.com>
2010-11-09 23:11:24 -0500

    Added SG as fixture user

--- frameworks/core-tasks/fixtures/user.js
+++ frameworks/core-tasks/fixtures/user.js
@@ -6,5 +6,6 @@ CoreTasks.User.FIXTURES = [
   { id: 1, loginName: "mgr", name: "Manager User", role: "_Manager" },
   { id: 2, loginName: "dev", name: "Developer User", role: "_Developer" },
   { id: 3, loginName: "tst", name: "Tester User", role: "_Tester" },
-  { id: 4, loginName: "guest", name: "Guest User", role: "_Guest" }
+  { id: 4, loginName: "guest", name: "Guest User", role: "_Guest" },
+  { id: 5, loginName: "SG", name: "Suvajit Gupta", role: "_Manager" }
 ];
