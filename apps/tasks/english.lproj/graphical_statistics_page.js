ad6589a9182969235446c30e652c4584c8580b3e
Suvajit Gupta <suvajit.gupta@eloqua.com>
2010-11-23 07:30:25 -0500

    Don't show task "type" chart when in Todos mode

--- apps/tasks/english.lproj/graphical_statistics_page.js
+++ apps/tasks/english.lproj/graphical_statistics_page.js
@@ -24,15 +24,16 @@ Tasks.graphicalStatisticsPage = SC.Page.create({
     displayProperties: ['statistics'],
     
     tasksStatistics: SC.View.design({
-      childViews: 'typeStatisticsChart priorityStatisticsChart statusStatisticsChart'.w(),
+      childViews: 'priorityStatisticsChart typeStatisticsChart statusStatisticsChart'.w(),
       isVisible: NO,
-      typeStatisticsChart: Sai.BarChartView.design({
-        layout: { top: 0, left: 0, width: 200, bottom: 0 },
-        dataAttrs: { horizontal: NO, barWidth: 25, colors: ['gold', 'red', 'gray'] }
-      }),
       priorityStatisticsChart: Sai.BarChartView.design({
-        layout: { top: 0, left: 200, width: 200, bottom: 0 },
+        layout: { top: 0, left: 0, width: 200, bottom: 0 },
         dataAttrs: { horizontal: NO, barWidth: 25, colors: ['black', 'gray', 'darkGray'] }
+      }),
+      typeStatisticsChart: Sai.BarChartView.design({
+        layout: { top: 0, left: 200, width: 200, bottom: 0 },
+        dataAttrs: { horizontal: NO, barWidth: 25, colors: ['gold', 'red', 'gray'] },
+        isVisibleBinding: SC.Binding.oneWay('Tasks.softwareMode')
       }),
       statusStatisticsChart: Sai.BarChartView.design({
         layout: { top: 0, left: 400, width: 220, bottom: 0 },
@@ -54,17 +55,17 @@ Tasks.graphicalStatisticsPage = SC.Page.create({
         if(Tasks.assignmentsController.get('displayMode') === Tasks.DISPLAY_MODE_TASKS) {
           this.setPath('tasksStatistics.isVisible', YES);
           this.setPath('teamStatistics.isVisible', NO);
+          this.setPath('tasksStatistics.priorityStatisticsChart.data', [ stats.highCount, stats.mediumCount, stats.lowCount ]);
+          this.setPath('tasksStatistics.priorityStatisticsChart.xaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'},
+                        labels: [ "_High".loc(), "_Medium".loc(), "_Low".loc() ] });
+          this.setPath('tasksStatistics.priorityStatisticsChart.yaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'}, buffer: 0.2, labels: YES,
+                       step: this._computeRange(this.getPath('tasksStatistics.priorityStatisticsChart.data')) });
           this.setPath('tasksStatistics.typeStatisticsChart.data', [ stats.featureCount, stats.bugCount, stats.otherCount ]);
           this.setPath('tasksStatistics.typeStatisticsChart.xaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'},
                        labels: [ "_Feature".loc(), "_Bug".loc(), "_Other".loc() ] });
           // FIXME: [SG/GD] make Sai support min/max settings for axes
           this.setPath('tasksStatistics.typeStatisticsChart.yaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'}, buffer: 0.2, labels: YES,
                        step: this._computeRange(this.getPath('tasksStatistics.typeStatisticsChart.data')) });
-          this.setPath('tasksStatistics.priorityStatisticsChart.data', [ stats.highCount, stats.mediumCount, stats.lowCount ]);
-          this.setPath('tasksStatistics.priorityStatisticsChart.xaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'},
-                        labels: [ "_High".loc(), "_Medium".loc(), "_Low".loc() ] });
-          this.setPath('tasksStatistics.priorityStatisticsChart.yaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'}, buffer: 0.2, labels: YES,
-                       step: this._computeRange(this.getPath('tasksStatistics.priorityStatisticsChart.data')) });
           this.setPath('tasksStatistics.statusStatisticsChart.data', [ stats.plannedCount, stats.activeCount, stats.doneCount, stats.riskyCount ]);
           this.setPath('tasksStatistics.statusStatisticsChart.xaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'},
                         labels: [ "_Planned".loc(), "_Active".loc(), "_Done".loc(), "_Risky".loc() ] });
