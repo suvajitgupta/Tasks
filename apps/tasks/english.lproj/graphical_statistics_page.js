958070e036d37827996416fdcb48e3f2be00316b
Suvajit Gupta <suvajit.gupta@eloqua.com>
2010-11-23 23:38:17 -0500

    Did some more work trying to use autoscaling & min/max overrides in Sai

--- apps/tasks/english.lproj/graphical_statistics_page.js
+++ apps/tasks/english.lproj/graphical_statistics_page.js
@@ -44,6 +44,7 @@ Tasks.graphicalStatisticsPage = SC.Page.create({
     teamStatistics: SC.View.design({
       childViews: 'loadingStatisticsChart'.w(),
       isVisible: NO,
+      // TODO: [SG/GD] use pie chart to show assignee loading distribution
       loadingStatisticsChart: Sai.BarChartView.design({
         dataAttrs: { horizontal: YES, barWidth: 20, colors: ['gray', 'green', 'blue', 'red'] } 
       })
@@ -58,18 +59,21 @@ Tasks.graphicalStatisticsPage = SC.Page.create({
           this.setPath('tasksStatistics.priorityStatisticsChart.data', [ stats.highCount, stats.mediumCount, stats.lowCount ]);
           this.setPath('tasksStatistics.priorityStatisticsChart.xaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'},
                         labels: [ "_High".loc(), "_Medium".loc(), "_Low".loc() ] });
-          this.setPath('tasksStatistics.priorityStatisticsChart.yaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'}, buffer: 0.2, labels: YES,
+          // FIXME: [GD] fix axis autoscaling & min/max override capability in Sai
+          this.setPath('tasksStatistics.priorityStatisticsChart.yaxis', { autoscale: YES, min: 0, max: stats.tasksCount,
+                                                                          color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'}, buffer: 0.2, labels: YES,
                        step: this._computeRange(this.getPath('tasksStatistics.priorityStatisticsChart.data')) });
           this.setPath('tasksStatistics.typeStatisticsChart.data', [ stats.featureCount, stats.bugCount, stats.otherCount ]);
           this.setPath('tasksStatistics.typeStatisticsChart.xaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'},
                        labels: [ "_Feature".loc(), "_Bug".loc(), "_Other".loc() ] });
-          // FIXME: [SG/GD] make Sai support min/max settings for axes
-          this.setPath('tasksStatistics.typeStatisticsChart.yaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'}, buffer: 0.2, labels: YES,
+          this.setPath('tasksStatistics.typeStatisticsChart.yaxis', { autoscale: YES, min: 0, max: stats.tasksCount,
+                                                                      color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'}, buffer: 0.2, labels: YES,
                        step: this._computeRange(this.getPath('tasksStatistics.typeStatisticsChart.data')) });
           this.setPath('tasksStatistics.statusStatisticsChart.data', [ stats.plannedCount, stats.activeCount, stats.doneCount, stats.riskyCount ]);
           this.setPath('tasksStatistics.statusStatisticsChart.xaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'},
                         labels: [ "_Planned".loc(), "_Active".loc(), "_Done".loc(), "_Risky".loc() ] });
-          this.setPath('tasksStatistics.statusStatisticsChart.yaxis', { color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'}, buffer: 0.2, labels: YES,
+          this.setPath('tasksStatistics.statusStatisticsChart.yaxis', { autoscale: YES, min: 0, max: stats.tasksCount,
+                                                                        color: Tasks.AXIS_COLOR, labelAttrs: {offset: -5, fontSize: '9'}, buffer: 0.2, labels: YES,
                        step: this._computeRange(this.getPath('tasksStatistics.statusStatisticsChart.data')) });
         }
         else { // displayMode === Tasks.DISPLAY_MODE_TEAM
@@ -86,7 +90,6 @@ Tasks.graphicalStatisticsPage = SC.Page.create({
       sc_super();
     },
     
-    // FIXME: [SG/GD] incorporate axis autoscaling capability in Sai
     _computeRange: function(values) {
       var max = Math.max.apply(null, values);
       var range = Math.floor(max/5);
