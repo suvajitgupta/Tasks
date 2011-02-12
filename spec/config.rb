b6399c939dc3e734709286baee69a5c9f1b74949
Suvajit Gupta <suvajit.gupta@eloqua.com>
2011-02-12 00:13:36 -0500

    Updated Lebowski scripts to catch up with latest Tasks login page

--- spec/config.rb
+++ spec/config.rb
@@ -6,29 +6,18 @@ App.define_path 'mainPane', 'mainPage.mainPane', MainPane
 
 USER_NAME = "SA"
 PASSWORD = ""
-USER_ROLE = "manager"
-NEW_PROJECT_DEFAULT_NAME = "new project"
-NEW_TASK_DEFAULT_NAME = "new task"
-NEWUSERLOGINNAME = "euser"
-NEWUSERFULLNAME = "Eager N. User"
-DEVELOPERUSERNAME = "Douglas Developer"
-IMPORTNEWTASK = "Fred
-- Imported: Feature: Medium Priority, Planned {2d} <SA> [DD] $Feature 
-| This is a description of the first imported task
-| This is the second line of the first tasks description there are no special characters"
-
+USER_ROLE = "Manager"
+NEWUSERLOGINNAME = "guest"
+NEWUSERFULLNAME = "Guest User"
 
 App.define_framework 'core_tasks', 'CoreTasks'
 
-App.define_path 'main_pane', 'mainPage.mainPane', MainPane
-App.define_path 'actions_button', 'main_pane.topBarView.actionsButton', ButtonView
-
 App.define_path 'login_panel', 'loginPage.panel', PanelPane
 App.define_paths_for 'login_panel' do |path|
-  path.define_path 'user_name_field', 'contentView.loginNameField', TextFieldView
+  path.define_path 'login_name_field', 'contentView.loginNameField', TextFieldView
   path.define_path 'password_field', 'contentView.passwordField', TextFieldView
-  path.define_path 'login_button', 'contentView.loginButton', ButtonView
-  path.define_path 'login_err_msg', 'contentView.loginErrorMessageLabel', LabelView 
+  path.define_path 'signin_button', 'contentView.signinButton', ButtonView
+  path.define_path 'login_error_message_label', 'contentView.loginErrorMessageLabel', LabelView 
   path.define_path 'guest_signup_button', 'contentView.guestSignupButton', ButtonView
 end
 
@@ -37,14 +26,17 @@ App.define_paths_for 'signup_panel' do |path|
   path.define_path 'signup_prompt', 'contentView.signupPrompt', LabelView
   path.define_path 'signup_button', 'contentView.signupButton', ButtonView
   path.define_path 'cancel_button', 'contentView.cancelButton', ButtonView
-  path.define_path 'fullNameLabel', 'contentView.userInformation.fullNameLabel', LabelView
-  path.define_path 'fullNameField', 'contentView.userInformation.fullNameField', TextFieldView
-  path.define_path 'loginNameLabel', 'contentView.userInformation.loginNameLabel', LabelView
-  path.define_path 'loginNameField', 'contentView.userInformation.loginNameField', TextFieldView
-  path.define_path 'roleLabel', 'contentView.userInformation.roleLabel', LabelView
-  path.define_path 'roleField', 'contentView.userInformation.roleField', 'SC.SelectButtonView' # was SelectFieldView 
-  path.define_path 'emailLabel', 'contentView.userInformation.emailLabel', LabelView
-  path.define_path 'emailField', 'contentView.userInformation.emailField', TextFieldView
-  path.define_path 'passwordLabel', 'contentView.userInformation.passwordLabel', LabelView
-  path.define_path 'passwordField', 'contentView.userInformation.passwordField', TextFieldView
+  path.define_path 'full_name_label', 'contentView.userInformation.fullNameLabel', LabelView
+  path.define_path 'full_name_field', 'contentView.userInformation.fullNameField', TextFieldView
+  path.define_path 'login_name_label', 'contentView.userInformation.loginNameLabel', LabelView
+  path.define_path 'login_name_field', 'contentView.userInformation.loginNameField', TextFieldView
+  path.define_path 'role_label', 'contentView.userInformation.roleLabel', LabelView
+  path.define_path 'role_field', 'contentView.userInformation.roleField', 'SC.SelectButtonView' # was SelectFieldView 
+  path.define_path 'email_label', 'contentView.userInformation.emailLabel', LabelView
+  path.define_path 'email_field', 'contentView.userInformation.emailField', TextFieldView
+  path.define_path 'password_label', 'contentView.userInformation.passwordLabel', LabelView
+  path.define_path 'password_field', 'contentView.userInformation.passwordField', TextFieldView
 end
+
+App.define_path 'main_pane', 'mainPage.mainPane', MainPane
+App.define_path 'actions_button', 'main_pane.masterDetailView.detailView.topToolbar.actionsButton', ButtonView
--- spec/login.rb
+++ spec/login.rb
@@ -6,19 +6,19 @@ describe "Login using bad user name should fail" do
 
 	it "will confirm that an error message is displayed on a failed login attempt" do
 	  App['login_panel'].should be_pane_attached
-	  App['login_panel.login_err_msg'].should have_value ''
-    App['login_panel.user_name_field',TextFieldView].type "InvalidLoginName"
+	  App['login_panel.login_error_message_label'].should have_value ''
+    App['login_panel.login_name_field',TextFieldView].type "InvalidLoginName"
     App['login_panel.password_field'].type "InvalidPassword"
-    App['login_panel.login_button'].click
+    App['login_panel.signin_button'].click
 	end
 
 	it "will clear the login and password fields, error message disappears" do
-	  puts "... login error = #{App['login_panel.login_err_msg'].value}"
+	  puts "... login error = #{App['login_panel.login_error_message_label'].value}"
 	  App.wait_until(60) do |it| 
-	    match = it['login_panel.login_err_msg'].value =~ /Login failed/i 
+	    match = it['login_panel.login_error_message_label'].value =~ /Login failed/i 
 	    not match.nil?
 	  end
-	  App['login_panel.user_name_field'].clear
+	  App['login_panel.login_name_field'].clear
 	  App['login_panel.password_field'].clear
 	end
 
@@ -28,35 +28,32 @@ end
 describe "Add a new user" do
 
   it "will confirm that there is a Guest Signup button" do
-    App['login_panel.guest_signup_button'].should have_title /Guest Signup/i 
+    App['login_panel.guest_signup_button'].should have_title /Guest Sign Up/i 
   end
 
   it "will open the Guest Signup panel" do
     App['login_panel.guest_signup_button'].click
     App['signup_panel'].wait_until {|it| it.is_pane_attached}
-    App['signup_panel.signup_prompt'].should have_value /Guest Signup/i
+    App['signup_panel.signup_prompt'].should have_value /Guest Sign Up/i
   	App['signup_panel.signup_button'].should_not be_enabled
-    App['signup_panel.signup_button'].should have_title /Signup/i
+    App['signup_panel.signup_button'].should have_title /Sign Up/i
     App['signup_panel.cancel_button'].should have_title /Cancel/i
   end
 
   it "will populate the user information in the Guest Signup panel and Cancel" do
-    App['signup_panel.fullNameLabel'].should have_value /Full Name:/i 
-    App['signup_panel.fullNameField'].type "#{NEWUSERFULLNAME}"
-    App['signup_panel.loginNameLabel'].should have_value /Login Name:/i
-    App['signup_panel.loginNameField'].type NEWUSERLOGINNAME
+    App['signup_panel.full_name_label'].should have_value /Full Name:/i 
+    App['signup_panel.full_name_field'].type "#{NEWUSERFULLNAME}"
+    App['signup_panel.login_name_label'].should have_value /Login Name:/i
+    App['signup_panel.login_name_field'].type NEWUSERLOGINNAME
     App['signup_panel.cancel_button'].click
     App['signup_panel'].wait_until {|it| !it.is_pane_attached}
     App['signup_panel'].should_not be_visible_in_window
     App['login_panel'].should be_visible_in_window
   end
 
-  it "will signup as a new user: #{NEWUSERLOGINNAME}" do
-    App['login_panel.guest_signup_button'].click
-    App['signup_panel'].wait_until {|it| it.is_pane_attached}
-    App['signup_panel.fullNameField'].type_keys "#{NEWUSERFULLNAME}"
-    App['signup_panel.loginNameField'].type NEWUSERLOGINNAME
-    App['signup_panel.signup_button'].click
+  it "will login as guest user: #{NEWUSERLOGINNAME}" do
+    App['login_panel.login_name_field'].type NEWUSERLOGINNAME
+    App['login_panel.signin_button'].click
   end
 
 	it "will confirm that the main pane is visible in window and the login panel is not" do
@@ -85,15 +82,6 @@ describe "Log the user out of the application" do
   
     pane.menu_items.click /^L/i
     
-    # pane = App.key_pane(MenuPane)
-    # menu_item = pane.menu_items.find_first({ :title => /Logout/i })
-    #     menu_item.click
-    #     # pane.menu_items.click('Logout')
-    #     10.times do
-    #       rp = App.responding_panes
-    #       rp.each { |p| puts "... responding pane class = #{p.class}, sc_pane = #{p.sc_class}"}
-    #       sleep 1
-    #     end
     App.wait_until {|it| it.key_pane.kind_of?(AlertPane) }
     pane = App.key_pane AlertPane
     pane.should have_message /Confirmation/i
@@ -101,43 +89,4 @@ describe "Log the user out of the application" do
     pane.click_button 'Yes'
   end
 
-  # it "will confirm that the login panel is visible in the window" do
-  #   App.wait_until {|it| !it.key_pane.kind_of?(AlertPane) }
-  #   App['login_panel'].wait_until {|it| it.is_pane_attached}
-  #   App['login_panel'].should be_visible_in_window
-  #   end
-
 end
-
-
-# describe "Log in to Application as '#{USER_NAME}" do
-#   
-#   it "will confirm that the login panel is visible in the window" do
-#     App['login_panel'].should be_visible_in_window
-#   end
-#   
-#   it "will confirm that there is no currentUser" do
-#      App['core_tasks'].wait_until { |it| !it.object?('currentUser') }
-#   end
-# 
-#   it "will log in to the application as '#{USER_NAME}'" do
-#       App['login_panel.user_name_field'].type USER_NAME
-#     App['login_panel.user_name_field'].type_key 'S'  # USER_NAME
-#       App['login_panel.user_name_field'].type_key_append 'A'
-#       App['login_panel.user_name_field'].type_key_append :backspace
-#       App['login_panel.user_name_field'].type_key_append 'A'
-#     App['login_panel.password_field'].type PASSWORD
-#     App['login_panel.login_button'].click
-#   end
-#   
-#   it "will confirm the current user logged in has a role of '#{USER_ROLE}'" do
-#     App['core_tasks'].wait_until { |it| it.object?('currentUser') }
-#     current_user = App['core_tasks.currentUser', :object]
-#     current_user.should have_role that_matches(/#{USER_ROLE}/i)
-#   end
-#   
-#   it "will confirm that the main pane is visible in window and the login panel is not" do
-#     App['login_panel'].should_not be_visible_in_window
-#     App['main_pane'].should be_visible_in_window
-#   end
-# end
