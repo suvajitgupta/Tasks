6e26e97b1d4e6b8d0447c2f4d877ca0f33ae4599
Suvajit Gupta <suvajit.gupta@eloqua.com>
2011-02-21 23:29:57 -0500

    Worked around ListView drop target selection messing up bug

--- apps/tasks/views/list.js
+++ apps/tasks/views/list.js
@@ -14,6 +14,68 @@
 Tasks.ListView = SC.ListView.extend(
 /** @scope Tasks.ListView.prototype */ {
   
+  // TODO: [SC] fix ListView to not use isSelected to indicate drop target - this causes selected items to appear unselected (delete next 2 functions when fixed)
+  showInsertionPoint: function(itemView, dropOperation) {
+    
+    var view = this._insertionPointView;
+    if (!view) {
+      view = this._insertionPointView = this.get('insertionPointView').create();
+    }
+    
+    var index  = itemView.get('contentIndex'),
+        len    = this.get('length'),
+        layout = SC.clone(itemView.get('layout')),
+        level  = itemView.get('outlineLevel'),
+        indent = itemView.get('outlineIndent') || 0,
+        group;
+
+    // show item indented if we are inserting at the end and the last item
+    // is a group item.  This is a special case that should really be 
+    // converted into a more general protocol.
+    if ((index >= len) && index>0) {
+      group = this.itemViewForContentIndex(len-1);
+      if (group.get('isGroupView')) {
+        level = 1;
+        indent = group.get('outlineIndent');
+      }
+    }
+    
+    if (SC.none(level)) level = -1;
+    
+    if (dropOperation & SC.DROP_ON) {
+      this.hideInsertionPoint();
+      itemView.$().addClass('drop-target');
+      this._lastDropOnView = itemView;
+    } else {
+
+      if (this._lastDropOnView) {
+        this._lastDropOnView.$().removeClass('drop-target');
+        this._lastDropOnView = null;
+      }
+      
+      if (dropOperation & SC.DROP_AFTER) layout.top += layout.height;
+      
+      layout.height = 2;
+      layout.right  = 0;
+      layout.left   = ((level+1) * indent) + 12;
+      delete layout.width;
+
+      view.set('layout', layout);
+      this.appendChild(view);
+    }
+  },
+  
+  hideInsertionPoint: function() {
+    if (this._lastDropOnView) {
+      this._lastDropOnView.$().removeClass('drop-target');
+      this._lastDropOnView = null;
+    }
+    
+    var view = this._insertionPointView;
+    if (view) view.removeFromParent().destroy();
+    this._insertionPointView = null;
+  },
+
   toggle: function(index) {
     var del     = this.get('contentDelegate'),
         content = this.get('content');
