1426ffab3cced3920217b8906fc54dfd128cbfd1
Suvajit Gupta <suvajit.gupta@eloqua.com>
2011-03-10 17:10:12 -0500

    Flip displayMode button text

--- apps/tasks/english.lproj/main_page.js
+++ apps/tasks/english.lproj/main_page.js
@@ -314,7 +314,7 @@ Tasks.mainPage = SC.Page.design({
             titleMinWidth: 0,
             toolTip: "_DisplayModeTooltip".loc(),
             titleBinding: SC.Binding.transform(function(value, binding) {
-                                                 return value? "_TASKS".loc() : "_TEAM".loc();
+                                                 return value? "_TEAM".loc() : "_TASKS".loc();
                                                }).from('Tasks.assignmentsController*displayMode'),
             action: function() { // toggle display mode
               Tasks.assignmentsController.set('displayMode', !Tasks.assignmentsController.get('displayMode'));
